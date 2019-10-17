7cf3310216584af91e9c3b5ab611b250beb1a52f
sumanth <sumanthkeith@gmail.com>
2019-10-17 12:50:04 +0530

    full and final update

--- limits-service/src/main/java/com/in28minutes/microservices/limitsservice/LimitsConfigurationController.java
+++ limits-service/src/main/java/com/in28minutes/microservices/limitsservice/LimitsConfigurationController.java
@@ -4,6 +4,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+
 @RestController
 public class LimitsConfigurationController {
 	
@@ -17,6 +19,17 @@ public class LimitsConfigurationController {
 		return new LimitConfiguration(1000,200);
 		
 	}
+	
+	@GetMapping("/fault-tolerance-example")
+	@HystrixCommand(fallbackMethod="fallBackRetriveConfiguration")
+	public LimitConfiguration retriveConfiguration() {
+		throw new RuntimeException("Not Available");
+	}
+	
+	public LimitConfiguration fallBackRetriveConfiguration() {
+		return new LimitConfiguration(9999, 9);
+	}
+
 	
 
 	@GetMapping("/applicationPropertiesLimits")
